4a58de11c07800c114c24ee80835208821171aab
Richard Kuhnt <r15ch13+git@gmail.com>
2016-07-21 22:56:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Audible Converter (AAX to M4A)
+Converts Audible AAX audiobooks to M4A while keeping chapters and the cover image intact.
+The cover can be added as a looped image so it will be shown while playing on [Plex Media Server](https://www.plex.tv/)
+
+# Usage
+```
+λ audible-converter -h
+
+  Usage: audible-converter [options] <file>
+
+
+  Commands:
+
+    list   list registered devices and their activation bytes (windows only)
+
+  Options:
+
+    -h, --help                      output usage information
+    -V, --version                   output the version number
+    -o, --output <filename>         output filename
+    -p, --path <path>               output path
+    -v, --verbose                   output detailed information
+    -a, --activation-bytes <value>  4 byte activation secret to decrypt Audible AAX files (e.g. 1CEB00DA)
+    -d, --device <number>           registered device number from which activation bytes are used
+    -l, --loop                      add looped cover image to Audiobook
+```
+
+## Example:
+
+```
+> audible-converter AgenttotheStars_ep6_A1SZN9UBXNTEA2.aax -p "Q:\Audible\converted" -d 1
+
+Haruki Murakami - 1Q84 (Buch 1 & 2) [2012] (Duration: 30h53m0s)
+Converting Audiobook (using 1CEB00DA for decryption) ... 100%
+Extracting Cover Image ... 100%
+
+Guillermo del Toro, Chuck Hogan - Das Blut [2010] (Duration: 12h4m17s)
+Converting Audiobook (using 1CEB00DA for decryption) ... 100%
+Extracting Cover Image ... 100%
+
+Stephen King - Der Anschlag [2012] (Duration: 31h50m22s)
+Converting Audiobook (using 1CEB00DA for decryption) ... 100%
+Extracting Cover Image ... 100%
+
+Max Brooks - Der Zombie Survival Guide. Überleben unter Untoten [2011] (Duration: 9h5m32s)
+Converting Audiobook (using 1CEB00DA for decryption) ... 100%
+Extracting Cover Image ... 100%
+
+Finished converting 4 Audiobooks!
+```
+
+# Requirements
+* [x] Installed Audible Download Manager (for getting your activation bytes)
+* [x] [ffmpeg](https://ffmpeg.org/) for converting
+
+# How it works
+* Extract cover image:
+  * `ffmpeg -y -i audiobook.aax cover.png`
+* Decrypted and convert to m4a:
+  * `ffmpeg -y -activation_bytes 1CEB00DA -i audiobook.aax -c:a copy -vn audiobook.m4a`
+* Add looped cover image:
+  * `ffmpeg -y -r 1 -loop 1 -i cover.png -i audioboob.m4a -c:a copy -shortest audiobook.m4v`
+
+# License
+[The MIT License (MIT)](http://r15ch13.mit-license.org/)
---
+++ index.js
@@ -0,0 +1,282 @@
+#!/usr/bin/env node
+'use strict'
+
+const _ = require('lodash')
+const colors = require('colors')
+const ffmpeg = require('fluent-ffmpeg')
+const glob = require('glob')
+const path = require('path')
+const program = require('commander')
+const sanitize = require('sanitize-filename')
+const winston = require('winston')
+
+const Promise = require('bluebird')
+const ffprobe = Promise.promisify(ffmpeg.ffprobe)
+const regeditList = Promise.promisify(require('regedit').list)
+
+const AudibleDevicesKey = 'HKLM\\SOFTWARE\\WOW6432Node\\Audible\\SWGIDMAP'
+const pkg = require('./package.json')
+
+const colorTheme = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  verbose: 'cyan',
+  debug: 'blue',
+  silly: 'magenta'
+}
+colors.setTheme(colorTheme)
+
+let increaseVerbosity = (v, total) => {
+  return total + 1
+}
+
+let toHex = (d) => {
+  // http://stackoverflow.com/a/13240395/2710739
+  return ('0' + (Number(d).toString(16))).slice(-2).toUpperCase()
+}
+
+let extractBytes = (byteArray) => {
+  return _.map(byteArray.slice(0, 4).reverse(), (n) => {
+    return toHex(n)
+  }).join('')
+}
+
+let fetchDeviceActivationBytes = () => {
+  return regeditList(AudibleDevicesKey).then((result) => {
+    let entries = _.map(result[AudibleDevicesKey].values, (n) => {
+      return extractBytes(n.value)
+    })
+
+    entries = _.reject(entries, (n) => {
+      return n.toUpperCase() === 'FFFFFFFF'
+    })
+
+    if (entries.length > 0) return entries
+    throw new Error('Could not find any Audible Activation Bytes!')
+  })
+}
+
+let fetchMetadata = (input) => {
+  return ffprobe(input)
+    .then((result) => {
+      return {
+        filetype: result.format.tags.major_brand ? result.format.tags.major_brand.trim().toLowerCase() : '',
+        artist: result.format.tags.artist ? result.format.tags.artist.trim() : '',
+        title: result.format.tags.title ? result.format.tags.title.trim() : '',
+        date: result.format.tags.date ? result.format.tags.date.trim() : '',
+        duration: `${Math.floor(result.format.duration / 3600)}h${Math.floor(result.format.duration % 3600 / 60)}m${Math.floor(result.format.duration % 3600 % 60)}s`,
+        durationRaw: Math.floor(result.format.duration)
+      }
+    })
+}
+
+let currentTimemarkToPercent = (timemark, total) => {
+  timemark = timemark.split(':')
+  return Math.floor(((timemark[0] * 3600) + (timemark[1] * 60) + Math.floor(timemark[2])) * 100 / total)
+}
+
+let extractCoverImage = (input, output) => {
+  return new Promise((resolve, reject) => {
+    ffmpeg(input)
+      .output(output)
+      .on('end', () => {
+        console.log('100%')
+        resolve()
+      })
+      .on('error', (err) => {
+        console.log('') // fix stdout
+        reject(err)
+      })
+      .on('start', (cmd) => {
+        process.stdout.write('Extracting Cover Image ... ')
+        winston.debug(cmd)
+      })
+      .on('progress', (msg) => {
+        winston.log('silly', msg)
+      })
+      .run()
+  })
+}
+
+let convertAudiobook = (input, output, activationBytes, duration) => {
+  return new Promise((resolve, reject) => {
+    ffmpeg(input)
+      .audioCodec('copy')
+      .noVideo()
+      .inputOptions([`-activation_bytes ${activationBytes}`])
+      .output(output)
+      .on('end', () => {
+        console.log('') // fix stdout
+        resolve()
+      })
+      .on('error', (err) => {
+        console.log('') // fix stdout
+        reject(err)
+      })
+      .on('start', (cmd) => {
+        winston.debug(cmd)
+      })
+      .on('progress', (msg) => {
+        process.stdout.write(`Converting Audiobook (using ${activationBytes} for decryption) ... ${currentTimemarkToPercent(msg.timemark, duration)}%` + '\r')
+      })
+      .run()
+  })
+}
+
+let addLoopedImage = (input, output, image, duration) => {
+  return new Promise((resolve, reject) => {
+    ffmpeg(input)
+      .input(image)
+      .inputOptions(['-r 1', '-loop 1'])
+      .audioCodec('copy')
+      .outputOptions(['-shortest'])
+      .output(output)
+      .on('end', () => {
+        console.log('') // fix stdout
+        resolve()
+      })
+      .on('error', (err) => {
+        console.log('') // fix stdout
+        reject(err)
+      })
+      .on('start', (cmd) => {
+        winston.debug(cmd)
+      })
+      .on('progress', (msg) => {
+        process.stdout.write(`Adding looped cover image to Audiobook ... ${currentTimemarkToPercent(msg.timemark, duration)}%` + '\r')
+      })
+      .run()
+  })
+}
+
+let converter = (inputFile) => {
+  winston.log('silly', inputFile)
+
+  let coverImage = null
+  let outputFile = null
+  let loopedFile = null
+  let duration = 0
+
+  return fetchMetadata(inputFile)
+    .then((metadata) => {
+      console.log(`${metadata.artist} - ${metadata.title} [${metadata.date}] (Duration: ${metadata.duration})`)
+      if (metadata.filetype !== 'aax') throw new Error('Not a valid AAX File!')
+      duration = metadata.durationRaw
+
+      let outputDirectory = program.path ? path.resolve(program.path) : path.dirname(inputFile)
+      let outputFilename = program.output ? sanitize(path.basename(program.output, path.extname(program.output))) : sanitize(`${metadata.artist} - ${metadata.title} [${metadata.date}]`)
+
+      coverImage = path.format({
+        dir: outputDirectory,
+        name: outputFilename,
+        ext: '.png'
+      })
+      winston.log('silly', coverImage)
+      outputFile = path.format({
+        dir: outputDirectory,
+        name: outputFilename,
+        ext: '.m4a'
+      })
+      winston.log('silly', outputFile)
+      loopedFile = path.format({
+        dir: outputDirectory,
+        name: outputFilename,
+        ext: '.m4v'
+      })
+      winston.log('silly', loopedFile)
+      return metadata
+    })
+    .then(() => {
+      return fetchDeviceActivationBytes()
+    })
+    .then((devices) => {
+      let bytes = devices[0] ? devices[0] : ''
+      bytes = program.activationBytes ? program.activationBytes : bytes
+      bytes = devices[program.device] ? devices[program.device] : bytes
+
+      if (program.device && !devices[program.device]) {
+        throw new Error(`Device Nr. ${program.device} not found! Please use the 'list' command to get your devices.`)
+      }
+      if (!bytes) {
+        throw new Error('Please provide activation bytes with -a <bytes> or select a device using -d <number>')
+      }
+      return convertAudiobook(inputFile, outputFile, bytes, duration)
+    })
+    .then(() => {
+      return extractCoverImage(inputFile, coverImage)
+    })
+    .then(() => {
+      if (program.loop) return addLoopedImage(outputFile, loopedFile, coverImage, duration)
+    })
+    .catch((err) => {
+      winston.error(err.message)
+      winston.debug(err)
+    })
+}
+
+let globPromise = (pattern, options) => {
+  return new Promise(function (resolve, reject) {
+    glob(pattern, options, function (err, files) {
+      return err === null ? resolve(files) : reject(err)
+    })
+  })
+}
+
+let main = function (inputFile) {
+  winston.level = _.findKey(winston.config.npm.levels, (o) => {
+    return o === program.verbose
+  })
+  winston.level = winston.level || 'error'
+
+  globPromise(inputFile, {})
+    .then((files) => {
+      return Promise.reduce(files, (total, file) => {
+        return converter(file).then(() => {
+          console.log('')
+          return ++total
+        })
+      }, 0)
+    })
+    .then((total) => {
+      console.log(`Finished converting ${total > 1 ? total : 'one'} Audiobook${total > 1 ? 's' : ''}!`)
+    })
+    .catch((err) => {
+      winston.error(err.message)
+      winston.debug(err)
+    })
+}
+
+program
+  .version(pkg.version)
+  .usage('[options] <file>')
+  .option('-o, --output <filename>', 'output filename')
+  .option('-p, --path <path>', 'output path')
+  .option('-v, --verbose', 'output detailed information', increaseVerbosity, 0)
+  .option('-a, --activation-bytes <value>', '4 byte activation secret to decrypt Audible AAX files (e.g. 1CEB00DA)', /^[A-Fa-f0-9]{8}$/i, false)
+  .option('-d, --device <number>', 'registered device number from which activation bytes are used')
+  .option('-l, --loop', 'add looped cover image to Audiobook')
+  .action(main)
+
+program
+  .command('list')
+  .description('list registered devices and their activation bytes (windows only)')
+  .action(() => {
+    fetchDeviceActivationBytes()
+      .then((result) => {
+        console.log('Activation bytes of registered devices:\n')
+        _.each(result, (v, k) => {
+          console.log(`Device ${k}: ${v}`)
+        })
+      })
+      .catch((err) => {
+        winston.error(err.message)
+        winston.debug(err)
+      })
+  })
+
+program.parse(process.argv)
+
+if (!process.argv.slice(2).length) {
+  program.help()
+}
